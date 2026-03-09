9d5e424265248d94b40debcdab4927010c9d018b
Deepak Erothi <deepakerothi7@gmail.com>
2026-03-09 15:57:27 +0530

    make a new server and connected to the mongooDb and postman

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const multer = require('multer');
+
 
 const app = express();
+app.use(express.json());
+
+app.post("/create-post", async (req , res) =>{
+    
+    console.log(req.body);
+    
+})
 
 module.exports = app;
